758bfd5f5bdbea5f5d13a0ee6f5e5f2980ef4595
MhammadHoseinAslani <m.hoseinaslani@gmail.com>
2021-12-14 20:18:59 +0330

    Added views to create/edit blog post inside the site.

--- blog/views.py
+++ blog/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
 from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
+
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
@@ -47,3 +48,5 @@ def post_remove(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.delete()
     return redirect('post_list')
+def index(request):
+    return render(request, 'blog/index.html', {'index':index})
